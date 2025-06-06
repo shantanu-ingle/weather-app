b74d72a6bea6e2c9f60f838815977a5a5fb88c42
shantanu-ingle <shantanuinglee1@gmail.com>
2025-06-06 14:09:35 +0530

    Fixed Error

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,6 +2,8 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
 import History from './pages/History';
 
+
+
 const App: React.FC = () => {
   return (
     <Router>
